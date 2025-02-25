1b5e8356b897cc79dd500aff3a530fde83b4cf80
Sai-Srevan2004 <22bd1a057l@gmail.com>
2025-02-25 21:26:45 +0530

    app.jsx merge conflicts removed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,12 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-<<<<<<< HEAD
 import SkillsExpirience from "./pages/Skills";
-=======
-import Skills from "./pages/Skills";
->>>>>>> ff2160ac4907242e49ca5f9ee3a2d894474fd25a
 import Achievements from "./pages/Achievements";
 
 function App() {
   return (
     <div className="bg-gray-900 text-white min-h-screen">
-<<<<<<< HEAD
       <div className="w-11/12 mx-auto">
         <Navbar />
         <Home />
@@ -26,16 +21,6 @@ function App() {
         <Contact />
         <Footer />
       </div>
-=======
-      <Navbar />
-      <Home />
-      <About />
-      <Skills/>
-      <Projects />
-      <Achievements/>
-      <Contact />
-      <Footer />
->>>>>>> ff2160ac4907242e49ca5f9ee3a2d894474fd25a
     </div>
   );
 }
