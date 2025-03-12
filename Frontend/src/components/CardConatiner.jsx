7e26c2330a727694d234b3def0bee87f14e416e7
Sai-Srevan2004 <22bd1a057l@gmail.com>
2025-03-12 23:32:51 +0530

    modified certifications page again

--- Frontend/src/components/CardConatiner.jsx
+++ Frontend/src/components/CardConatiner.jsx
@@ -49,7 +49,7 @@ function CardContainer() {
   ];
 
   return (
-    <div className="relative mt-24 sm:mt-24 md:mt-10 w-[950px]">
+    <div className="relative mt-24 sm:mt-24 md:mt-10 w-full lg:w-[900px]">
       {/* Wrap all Cards inside a single AwesomeSlider */}
       <AwesomeSlider className="h-[420px] md:h-[490px]">
         {projects.map((project, index) => (
